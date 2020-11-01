f567d821db85a51f4bc3ada83051b6f5f89e6ed4
RomashkaStepanov <by.chamomilla@gmail.com>
2020-11-01 14:42:26 +0300

    и снова гит и снова сложно

---
+++ homework/src/HW4/Employee.java
@@ -0,0 +1,17 @@
+public class Employee {
+
+    String name;
+    int age;
+    String position;
+    String phoneNum;
+    int salary;
+
+    Employee(String name, int age, String position, String phoneNum, int salary){
+        this.name = name;
+        this.age = age;
+        this.position = position;
+        this.phoneNum = phoneNum;
+        this.salary = salary;
+    }
+
+}
